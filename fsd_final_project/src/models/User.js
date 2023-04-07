9c088d4a0253c2528988c0d709c938905de83588
Jackie Pei <jackiew@berkeley.edu>
2023-04-07 13:58:15 -0700

    duplicated hw7 structure

---
+++ fsd_final_project/src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  shoppinglist: {
+    type: Array,
+    required: false,
+  },
+  // Add more fields for information on the user.
+});
+
+// export model user with UserSchema
+module.exports = mongoose.model("user", UserSchema);
