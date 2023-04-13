c0db57f4b016ea466b656c17342e043b062d68f0
Tim <timothy.wang@berkeley.edu>
2023-04-12 20:45:44 -0700

    fix database files

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Replace this with your MONGOURI.
-const MONGOURI = "mongodb://127.0.0.1:27017/hw9";
+const MONGOURI = "mongodb://127.0.0.1:27017/__(insert db name)___";
 const InitiateMongoServer = async () => {
   try {
     await mongoose.connect(MONGOURI, {
